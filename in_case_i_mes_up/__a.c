fc571ce1641813b26f8e212077a20a0c39bdf24c
bi3mer <colan@comcast.net>
2025-11-03 11:57:58 -0500

    updated a* mess up code

--- in_case_i_mes_up/__a.c
+++ in_case_i_mes_up/__a.c
@@ -1,51 +1,54 @@
-if (state->found || da_length(state->queue) == 0)
-    return false; // done processing
-
-const AStarNode node = state->queue[0];
-const size_t node_id = grid_index(state->grid, node.point);
-da_pop_start(state->queue);
-
-if (point_equals(node.point, state->tgt))
+bool astar_step(AStarState *state)
 {
-    Point cur = node.point;
-    state->found = true;
+    if (state->found || da_length(state->queue) == 0)
+        return false; // done processing
 
-    while (!point_equals(cur, state->src))
+    AStarNode node = state->queue[0];
+    da_pop_start(state->queue);
+    const size_t node_id = grid_index(state->grid, node.point);
+
+    if (point_equals(node.point, state->tgt))
     {
-        *(Point *)da_append((void **)&state->path) = cur;
-        cur = state->came_from[grid_index(state->grid, cur)];
+        state->found = true;
+        Point p = node.point;
+
+        while (!point_equals(p, state->src))
+        {
+            *(Point *)da_append((void **)&state->path) = p;
+            p = state->came_from[grid_index(state->grid, p)];
+        }
+
+        *(Point *)da_append((void **)&state->path) = state->src;
+        da_reverse(state->path);
+
+        return false; // done processing
     }
 
-    *(Point *)da_append((void **)&state->path) = cur;
-    da_reverse(state->path);
+    Point neighbors[4];
+    const size_t num_neighbors =
+        grid_neighbors(state->grid, node.point, neighbors);
 
-    return false; // done processing
-}
-
-Point neighbors[4];
-const size_t num_neighbors = grid_neighbors(state->grid, node.point, neighbors);
-
-for (size_t i = 0; i < num_neighbors; ++i)
-{
-    const Point n = neighbors[i];
-    const size_t id = grid_index(state->grid, n);
-
-    const Terrain_Type terrain = grid_at(state->grid, n);
-    const float move_cost = terrain_cost(terrain);
-
-    const float new_cost = state->cost_so_far[node_id] + move_cost;
-
-    if (state->came_from[id].x == -1 || new_cost < state->cost_so_far[id])
+    for (size_t i = 0; i < num_neighbors; ++i)
     {
-        state->cost_so_far[id] = new_cost;
-        state->came_from[id] = node.point;
+        const size_t id = grid_index(state->grid, neighbors[i]);
+        const float tc = terrain_cost(state->grid->grid[id]);
+        const float cost = tc + state->cost_so_far[node_id];
 
-        const float priority = new_cost + state->heuristic(n, state->tgt);
-        const size_t insert_idx = da_priority_insert(
-            (void **)&state->queue, (size_t)priority, astar_compare);
+        if (state->came_from[id].x == -1 || cost < state->cost_so_far[id])
+        {
+            const size_t n_id = grid_index(state->grid, neighbors[i]);
+            state->came_from[n_id] = node.point;
+            state->cost_so_far[n_id] = cost;
 
-        state->queue[insert_idx] = (AStarNode){n, priority};
+            const float priority =
+                cost + heuristic(state->heuristic, neighbors[i], state->tgt);
+
+            AStarNode *a = da_priority_insert((void **)&state->queue, priority,
+                                              astar_compare);
+            a->priority = priority;
+            a->point = neighbors[i];
+        }
     }
-}
 
-return true;
+    return true; // Still processing}
+}
